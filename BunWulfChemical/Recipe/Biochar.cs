e4c9dee3aaa81b9ecf71981b50f2213898eae495
Kai Siren <coilysiren@gmail.com>
2024-09-29 14:42:36 -0700

    update biochar

--- BunWulfChemical/Recipe/Biochar.cs
+++ BunWulfChemical/Recipe/Biochar.cs
@@ -26,33 +26,47 @@ namespace Eco.Mods.TechTree
     {
         public BiocharRecipe()
         {
-            this.Recipes = new List<Recipe>
-            {
-                new Recipe(
-                    "Biochar",
-                    Localizer.DoStr("Biochar"),
-                    new IngredientElement[]
-                    {
-                        new IngredientElement("Coal", 4, true),
-                        new IngredientElement("Crop", 10, typeof(FarmingSkill), typeof(FarmingLavishResourcesTalent)),
-                    },
-                    new CraftingElement[] {
-                        new CraftingElement<CharcoalItem>(5),
-                        new CraftingElement<OilItem>(typeof(FarmingSkill), 2, typeof(FarmingLavishResourcesTalent)),
-                    }
-                )
-            };
-            this.ExperienceOnCraft = 1;
-            this.LaborInCalories = CreateLaborInCaloriesValue(10, typeof(FarmingSkill));
-            this.CraftMinutes = CreateCraftTimeValue(
-                typeof(BiocharRecipe),
-                0.2f,
-                typeof(FarmingSkill),
-                typeof(FarmingFocusedSpeedTalent)
+            var recipe = new Recipe();
+            recipe.Init(
+                name: "Biochar",
+                displayName: Localizer.DoStr("Biochar"),
+                ingredients: new List<IngredientElement>
+                {
+                    new IngredientElement(typeof(CharcoalItem), 4, typeof(FarmingSkill)),
+                    new IngredientElement("Crop", 10, typeof(FarmingSkill), typeof(FarmingLavishResourcesTalent)),
+                },
+                items: new List<CraftingElement>
+                {
+                    new CraftingElement<CharcoalItem>(5),
+                    new CraftingElement<OilItem>(2),
+                }
             );
-            this.Initialize(Localizer.DoStr("Biochar"), typeof(BiocharRecipe));
-            CraftingComponent.AddRecipe(typeof(StoveObject), this);
-            CraftingComponent.AddRecipe(typeof(KilnObject), this);
+            this.Recipes = new List<Recipe> { recipe };
+            this.ExperienceOnCraft = 1;
+            this.LaborInCalories = CreateLaborInCaloriesValue(25, typeof(FarmingSkill));
+            this.CraftMinutes = CreateCraftTimeValue(
+                beneficiary: typeof(BiocharRecipe),
+                start: 0.2f,
+                skillType: typeof(FarmingSkill),
+                typeof(FarmingFocusedSpeedTalent)
+                typeof(FarmingParallelSpeedTalent)
+            );
+            this.ModsPreInitialize();
+            this.Initialize(
+                displayText: Localizer.DoStr("Biochar"),
+                recipeType: typeof(BiocharRecipe),
+            );
+            this.ModsPostInitialize();
+            CraftingComponent.AddRecipe(
+                tableType: typeof(BakeryOvenObject),
+                recipe: this,
+            );
+            CraftingComponent.AddRecipe(
+                tableType: typeof(KilnObject),
+                recipe: this,
+            );
         }
+        partial void ModsPreInitialize();
+        partial void ModsPostInitialize();
     }
 }
